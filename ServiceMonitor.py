d2bb0da44b4601e1d944da777a4bb32596c2666b
sela27 <sela@goldenberg.xyz>
2019-04-10 13:15:22 +0300

    version with import datetime

--- ServiceMonitor.py
+++ ServiceMonitor.py
@@ -2,12 +2,8 @@ import psutil
 import time
 import os
 from time import gmtime, strftime
-import datetime 
+import datetime
 #import sys
-
-
-#print(psutil.test())
-#print("please choose State: \n 1 - Monitor state \n 2 - manual state")
 
 def writeToServiceList():
 	serviceList = open("serviceList" , "a")
@@ -16,7 +12,7 @@ def writeToServiceList():
 	serviceList.write(str(datetime.datetime.now()) + "\n")
 	for proc in psutil.process_iter():
 		try:
-			pinfo = proc.as_dict(attrs=['pid' ,'name', 'username' , 'create_time' ,'status'])
+			pinfo = proc.as_dict(attrs=['pid' ,'name', 'username' ,'status'])
 		except psutil.NoSuchProcess:
 			pass
         	else:
@@ -34,14 +30,14 @@ def writeToStatusLog(old_process_dict , new_process_dict):
 	for oldproc,info in old_process_dict.items():
 		if oldproc not in new_process_dict:
 			statuslog.write(str(oldproc) + str(info) + " has died\n")
-			statuslog.write("time of death: " + str(now) + "\n")
+			statuslog.write("time: " + str(now) + "\n")
 			print("pid: " +str(oldproc) +" "+ str(info) + " has died")
 			now = time.ctime(time.time())
 
 	for newproc,info in new_process_dict.items():
 		if newproc not in old_process_dict:
 			statuslog.write(str(newproc) + str(info) + " was created\n")
-			statuslog.write("time of birth: " + str(now) + "\n")
+			statuslog.write("time: " + str(now) + "\n")
 			print("pid: " + str(newproc) + str(info) + " was created")
 			now = time.ctime(time.time())
 	statuslog.close()
@@ -60,16 +56,15 @@ if x == 1:
 
 elif x == 2:
 	#TODO: add input for dates
-	serviceList = open("serviceList" , "r")
+	serviceList = open("Status_Log.txt" , "r")
 	for line in serviceList.readline():
-		if line[0] != line[1] != line[2] != '*':
-			pass
-		else:
-			line = serviceList.readline()
+		if line.startswith("time:"):
+			time = line[6:]
 			date = datetime.datetime.strptime(line, '%Y-%m-%d %H:%M:%S.%f')
 			#check if date is after the given time and if so take this sample
+		else:
+			pass
+			
 
 else:
 	print("WORNG")
-
-
